5eb7ddd7eb7cd4dd96e6978a9bdf6e19e632e572
Witsarut Wongsim <row3no6@gmail.com>
2023-05-04 15:00:54 +0700

    Add files via upload

---
+++ files/postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE food (
+    food       VARCHAR(120)
+);
+INSERT INTO food (food) values ('Stranger Things');
+INSERT INTO food (food) values ('Black Mirror');
+INSERT INTO food (food) values ('The Office');
+
+ 
